1e6cc52b263e9c3ac3eb23715e863f3cadc59dac
guohuageng <guohuageng@gmail.com>
2024-10-15 23:13:44 +0800

    测试右舵车识别开关

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -71,6 +71,7 @@ def manager_init() -> None:
     ("EnhancedScc", "0"),
     ("FeatureStatus", "1"),
     ("HandsOnWheelMonitoring", "0"),
+    ("IsRhdDetected", "0"),
     ("HasAcceptedTermsSP", "0"),
     ("HideVEgoUi", "0"),
     ("HyundaiCruiseMainDefault", "0"),
